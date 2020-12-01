1756634b0001a606467e34c8eea0fc940772e338
Fabien Brulport <fabien.brulport@gmail.com>
2020-12-01 17:32:47 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import argparse
+from importlib import import_module
+
+
+def run_main(problem_number):
+    module = import_module(f"code.solution_{problem_number}")
+    module.main()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("problem_number")
+    args = parser.parse_args()
+    run_main(args.problem_number)
